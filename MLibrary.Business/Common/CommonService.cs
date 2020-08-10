9c919a3ec76882d28f6eb418fa10f2412cbef9a3
MustafaColakoglu <mustafacolakoglu7@gmail.com>
2020-08-10 12:09:30 +0300

    crypto class added

--- MLibrary.Business/Common/CommonService.cs
+++ MLibrary.Business/Common/CommonService.cs
@@ -1,11 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 
 namespace MLibrary.Business.Common
 {
-    class CommonService
+    public static class CommonService
     {
         public static string GenerateRandomString(int length)
         {
@@ -13,6 +16,64 @@ namespace MLibrary.Business.Common
             const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789qwertyuıopasdfghjklzxcvbnm.,_";
             return new string(Enumerable.Repeat(chars, length)
               .Select(s => s[random.Next(s.Length)]).ToArray());
+        }
+
+        public static DataTable ConvertToDatatable<T>(List<T> data)
+        {
+            PropertyDescriptorCollection props = TypeDescriptor.GetProperties(typeof(T));
+            DataTable table = new DataTable();
+            for (int i = 0; i < props.Count; i++)
+            {
+                PropertyDescriptor prop = props[i];
+                if (prop.PropertyType.IsGenericType && prop.PropertyType.GetGenericTypeDefinition() == typeof(Nullable<>))
+                    table.Columns.Add(prop.DisplayName, prop.PropertyType.GetGenericArguments()[0]);
+                else
+                    table.Columns.Add(prop.DisplayName, prop.PropertyType);
+            }
+
+            object[] values = new object[props.Count];
+            foreach (T item in data)
+            {
+                for (int i = 0; i < values.Length; i++)
+                {
+                    values[i] = props[i].GetValue(item);
+                }
+                table.Rows.Add(values);
+            }
+            return table;
+        }
+
+        public static bool IsValidEmail(string email)
+        {
+            if (string.IsNullOrWhiteSpace(email))
+                return false;
+
+
+
+            try
+            {
+                bool control = Regex.IsMatch(email,
+                    @"^(?("")("".+?(?<!\\)""@)|(([0-9a-z]((\.(?!\.))|[-!#\$%&'\*\+/=\?\^`\{\}\|~\w])*)(?<=[0-9a-z])@))" +
+                    @"(?(\[)(\[(\d{1,3}\.){3}\d{1,3}\])|(([0-9a-z][-0-9a-z]*[0-9a-z]*\.)+[a-z0-9][\-a-z0-9]{0,22}[a-z0-9]))$",
+                    RegexOptions.IgnoreCase, TimeSpan.FromMilliseconds(250));
+
+                string[] turkishChar = new[] { "ğ", "ç", "ş", "ü", "ö", "ı", "Ğ", "Ç", "Ş", "Ü", "Ö", "İ" };
+
+                if (!control)
+                    return false;
+
+                if (control && turkishChar.Any(email.Contains))
+                    return false;
+
+
+            }
+            catch (RegexMatchTimeoutException)
+            {
+                return false;
+            }
+
+
+            return true;
         }
     }
 }
---
+++ MLibrary.Business/Cryptography/CryptographyService.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace MLibrary.Business.Cryptography
+{
+    public static class CryptographyService
+    {
+        public static string Encrypt(string sIn, string text)
+        {
+            try
+            {
+                TripleDESCryptoServiceProvider DES = new TripleDESCryptoServiceProvider();
+                MD5CryptoServiceProvider hashMD5 = new MD5CryptoServiceProvider();
+                DES.Key = hashMD5.ComputeHash(System.Text.ASCIIEncoding.ASCII.GetBytes(text));
+                DES.Mode = CipherMode.ECB;
+                ICryptoTransform DESEncrypt = DES.CreateEncryptor();
+                byte[] Buffer = System.Text.ASCIIEncoding.ASCII.GetBytes(sIn);
+                return Convert.ToBase64String(DESEncrypt.TransformFinalBlock(Buffer, 0, Buffer.Length));
+            }
+            catch (Exception ex)
+            {
+                return sIn;
+            }
+        }
+
+        public static string Decrypt(string sOut, string text)
+        {
+            try
+            {
+                TripleDESCryptoServiceProvider DES = new TripleDESCryptoServiceProvider();
+                MD5CryptoServiceProvider hashMD5 = new MD5CryptoServiceProvider();
+                DES.Key = hashMD5.ComputeHash(System.Text.ASCIIEncoding.ASCII.GetBytes(text));
+                DES.Mode = CipherMode.ECB;
+                ICryptoTransform DESDecrypt = DES.CreateDecryptor();
+                byte[] Buffer = Convert.FromBase64String(sOut);
+                return System.Text.ASCIIEncoding.ASCII.GetString(DESDecrypt.TransformFinalBlock(Buffer, 0, Buffer.Length));
+            }
+            catch (Exception ex)
+            {
+                return sOut;
+            }
+        }
+    }
+}
